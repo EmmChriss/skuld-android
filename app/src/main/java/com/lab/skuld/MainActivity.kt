aae9f0bdd19459640c3d18e0c54d36139bb63d45
EEtele <mihaly.etele@student.ms.sapientia.ro>
2022-12-27 23:11:40 +0200

    Drawer improvements

--- app/src/main/java/com/lab/skuld/MainActivity.kt
+++ app/src/main/java/com/lab/skuld/MainActivity.kt
@@ -1,17 +1,8 @@
 package com.lab.skuld
 
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.FirebaseFirestoreSettings
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import com.lab.skuld.ui.App
 
 class MainActivity : ComponentActivity() {
@@ -20,3 +11,4 @@ class MainActivity : ComponentActivity() {
         setContent { App() }
     }
 }
+
--- app/src/main/java/com/lab/skuld/ui/App.kt
+++ app/src/main/java/com/lab/skuld/ui/App.kt
@@ -1,7 +1,5 @@
 package com.lab.skuld.ui
 
-import android.annotation.SuppressLint
-import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,19 +8,34 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.Divider
+import androidx.compose.material.FabPosition
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.runtime.*
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthSettings
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.lab.skuld.ui.screens.ShowLoginScreen
@@ -30,10 +43,6 @@ import com.lab.skuld.ui.screens.ShowNewNoteScreen
 import com.lab.skuld.ui.screens.ShowTasksScreen
 import com.lab.skuld.ui.theme.SkuldFrontendTheme
 import kotlinx.coroutines.launch
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 
 // Main Composable entry-point
 @Composable
@@ -44,6 +53,7 @@ fun App() {
         }
     }
 }
+
 
 @Composable
 fun Auth(content: @Composable () -> Unit) {
@@ -58,11 +68,12 @@ fun Auth(content: @Composable () -> Unit) {
         }
     }
 
-    if (!loggedIn) {
+    if (false) {
         ShowLoginScreen()
     } else {
         content()
     }
+    
 }
 
 sealed class Screen(val title: String, val content: @Composable () -> Unit, val onBack: Screen? = null ) {
@@ -136,11 +147,15 @@ fun Navigation() {
     Scaffold(
         scaffoldState = scaffoldState,
         drawerContent = {
-            menuOptions.forEach {
-                MenuOption(it.title) { navigateTo(it) }
-            }
-            MenuOption("Log out") {
-                openLogoutDialog = true
+            Column(modifier = Modifier.fillMaxWidth(),horizontalAlignment = Alignment.CenterHorizontally) {
+                Text("Skuld", modifier = Modifier.padding(16.dp), fontSize = 40.sp, fontWeight = FontWeight.Bold)
+                Divider(modifier = Modifier.padding(7.dp))
+                menuOptions.forEach {
+                    MenuOption(it.title) { navigateTo(it) }
+                }
+                MenuOption("Log out") {
+                    openLogoutDialog = true
+                }
             }
         },
         topBar = {
@@ -199,7 +214,7 @@ fun Menu() {
 
 @Composable
 fun MenuOption(text: String, onClick: () -> Unit) {
-    Button(onClick = onClick, Modifier.fillMaxWidth()) { Text(text) }
+    Button(onClick = onClick, Modifier.fillMaxWidth(0.7f)) { Text(text) }
 }
 
 @Composable
